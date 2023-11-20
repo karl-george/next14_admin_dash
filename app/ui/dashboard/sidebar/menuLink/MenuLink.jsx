5eba062df4077da73ab5f172fcb827ff1f1bf46d
Karl George <karlgeorge87@gmail.com>
2023-11-20 12:47:34 +0100

    Style menu links

--- app/ui/dashboard/sidebar/menuLink/MenuLink.jsx
+++ app/ui/dashboard/sidebar/menuLink/MenuLink.jsx
@@ -1,9 +1,19 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './menulink.module.css';
 
 const MenuLink = ({ item }) => {
+  const pathname = usePathname();
+
   return (
-    <Link href={item.path} className={styles.container}>
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathname === item.path && styles.active
+      }`}
+    >
       {item.icon}
       {item.title}
     </Link>
