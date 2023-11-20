48c6134f62f97f9e16b7071ba4419e692cdb45a3
Karl George <karlgeorge87@gmail.com>
2023-11-20 11:24:49 +0100

    Setup site structure

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import Navbar from '../ui/dashboard/navbar/Navbar';
+import Sidebar from '../ui/dashboard/sidebar/Sidebar';
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default layout;
