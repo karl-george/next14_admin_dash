f119d4a212344468c415a0eb4f80c59d815de178
Karl George <karlgeorge87@gmail.com>
2023-11-20 11:55:46 +0100

    Add sidebar

--- app/ui/dashboard/sidebar/Sidebar.jsx
+++ app/ui/dashboard/sidebar/Sidebar.jsx
@@ -1,5 +1,109 @@
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+} from 'react-icons/md';
+import styles from './sidebar.module.css';
+import MenuLink from './menuLink/MenuLink';
+import Image from 'next/image';
+
+const menuItems = [
+  {
+    title: 'Pages',
+    list: [
+      {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <MdDashboard />,
+      },
+      {
+        title: 'Users',
+        path: '/dashboard/users',
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: 'Products',
+        path: '/dashboard/products',
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: 'Transactions',
+        path: '/dashboard/transactions',
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: 'Analytics',
+    list: [
+      {
+        title: 'Revenue',
+        path: '/dashboard/revenue',
+        icon: <MdWork />,
+      },
+      {
+        title: 'Reports',
+        path: '/dashboard/reports',
+        icon: <MdAnalytics />,
+      },
+      {
+        title: 'Teams',
+        path: '/dashboard/teams',
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: 'User',
+    list: [
+      {
+        title: 'Settings',
+        path: '/dashboard/settings',
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: 'Help',
+        path: '/dashboard/help',
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          src='/noavatar.png'
+          alt='profile'
+          width={50}
+          height={50}
+          className={styles.userImage}
+        />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>John Doe</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((item) => (
+          <li key={item.title}>
+            <span className={styles.cat}>{item.title}</span>
+            {item.list.map((listItem) => (
+              <MenuLink key={listItem.title} item={listItem} />
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
