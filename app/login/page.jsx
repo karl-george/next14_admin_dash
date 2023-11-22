fa78ab285acb94425eb091524b81361d763c1c7a
Karl George <karlgeorge87@gmail.com>
2023-11-22 11:09:26 +0100

    Add login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,21 @@
+import styles from '@/app/ui/login/login.module.css';
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <form action='' className={styles.form}>
+        <h1>Login</h1>
+        <input type='text' placeholder='Username' name='username' required />
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          required
+        />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default page;
