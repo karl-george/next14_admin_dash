9c8000ef81b124805e28a43049397067cdf1508e
Karl George <karlgeorge87@gmail.com>
2023-11-23 11:14:23 +0100

    Update update user

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,36 +1,50 @@
+import { updateUser } from '@/app/lib/actions';
+import { fetchUser } from '@/app/lib/data';
 import styles from '@/app/ui/dashboard/users/singleUser.module.css';
 import Image from 'next/image';
 
-const page = () => {
+const page = async ({ params }) => {
+  const { id } = params;
+  const user = await fetchUser(id);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.imgContainer}>
-          <Image src='/noavatar.png' alt='user' fill />
+          <Image src={user.img || '/noavatar.png'} alt='user' fill />
         </div>
-        John Doe
+        {user.username}
       </div>
       <div className={styles.formContainer}>
-        <form action='' className={styles.form}>
+        <form action={updateUser} className={styles.form}>
+          <input type='hidden' name='id' value={user.id} />
           <label htmlFor='username'>Username</label>
-          <input type='text' name='username' placeholder='John Doe' />
+          <input type='text' name='username' placeholder={user.username} />
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' placeholder='JohnDoe@gmail.com' />
+          <input type='email' name='email' placeholder={user.email} />
           <label htmlFor='password'>Password</label>
           <input type='password' name='password' />
           <label htmlFor='username'>Phone</label>
-          <input type='text' name='phone' placeholder='+123456789' />
+          <input type='text' name='phone' placeholder={user.phone} />
           <label htmlFor='address'>Adress</label>
-          <textarea type='text' name='address' placeholder='New York' />
+          <textarea type='text' name='address' placeholder={user.address} />
           <label htmlFor='isAdmin'>isAdmin?</label>
           <select name='isAdmin' id='isAdmin'>
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value={true} selected={user.isAdmin}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isAdmin}>
+              No
+            </option>
           </select>
           <label htmlFor='isActive'>isActive?</label>
           <select name='isActive' id='isActive'>
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value={true} selected={user.isActive}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isActive}>
+              No
+            </option>
           </select>
           <button>Update</button>
         </form>
