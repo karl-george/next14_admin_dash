ae5205601a59458da0e39d2bd52f46a3a506d9d0
Karl George <karlgeorge87@gmail.com>
2023-11-23 10:57:35 +0100

    Add delete product

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,3 +1,4 @@
+import { deleteProduct } from '@/app/lib/actions';
 import { fetchProducts } from '@/app/lib/data';
 import Pagination from '@/app/ui/dashboard/pagination/Pagination';
 import styles from '@/app/ui/dashboard/products/products.module.css';
@@ -56,11 +57,12 @@ const page = async ({ searchParams }) => {
                       View
                     </button>
                   </Link>
-                  <Link href={'/'}>
+                  <form action={deleteProduct}>
+                    <input type='hidden' value={product.id} name='id' />
                     <button className={`${styles.button} ${styles.delete}`}>
                       Delete
                     </button>
-                  </Link>
+                  </form>
                 </div>
               </td>
             </tr>
--- app/lib/actions.js
+++ app/lib/actions.js
@@ -68,3 +68,17 @@ export const addProduct = async (formData) => {
   revalidatePath('/dashboard/products');
   redirect('/dashboard/products');
 };
+
+export const deleteProduct = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    await Product.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to delete product');
+  }
+
+  revalidatePath('/dashboard/products');
+};
